d18aabb651a88503946391fbf02e19839b1fe130
Manoj Talwar <talwarmanoj@users.noreply.github.com>
2020-07-24 11:33:05 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ chmod a+x /tmp/loop.sh
 
 nohup /tmp/loop.sh&
 
-python ftransfer.py 8080 samplefiles:transferme 1> /opt/app-root/server.log
+python ftransfer.py 8080 samplefiles:transferme 1> /opt/app-root/server.log 2> /opt/app-root/server.log
