80ef05ecad6add412b7ce4adca7d9857673a7d7e
Manoj Talwar <talwarmanoj@users.noreply.github.com>
2020-07-24 10:10:05 +0100

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,1 +1,1 @@
-while true;do rsync /opt/app-root/src/*mp3 /opt/app-root/src/samplefiles/ 2> /dev/null; sleep 300; done
+while true;do rsync /opt/app-root/src/*mp3 /opt/app-root/src/persistent/ 2> /dev/null; sleep 300; done
